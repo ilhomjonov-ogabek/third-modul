82955b1ae6a3c7642fc257e4ad0c07ab9cc5be3c
Ogabek <ilhomjonovo200@gmail.com>
2025-09-23 21:48:39 +0500

    first commit

---
+++ src/one_lesson/OneMain.java
@@ -0,0 +1,9 @@
+package one_lesson;
+
+public class OneMain {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/twoo_lesson/SecondMain.java
@@ -0,0 +1,8 @@
+package twoo_lesson;
+
+public class SecondMain {
+
+  public static void main(String[] args) {
+
+  }
+}
