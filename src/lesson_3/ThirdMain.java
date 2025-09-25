acd9d766045b713f426bceec191427bc11b402f4
Ogabek <ilhomjonovo200@gmail.com>
2025-09-25 21:45:17 +0500

    first commit

---
+++ src/lesson_3/ThirdMain.java
@@ -0,0 +1,9 @@
+package lesson_3;
+
+public class ThirdMain {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/lesson_4/homeworks/homework2/SinglyLinkedList.java
@@ -0,0 +1,162 @@
+package lesson_4.homeworks.homework2;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SinglyLinkedList<E> {
+
+  private Node<E> head;
+  private int size;
+
+
+  public void addFirst( E element ) {
+    Node<E> node = new Node<>(element);
+    node.next = head;
+    head = node;
+    size++;
+  }
+
+  public void add( E element ) {
+    var node = new Node<>( element );
+    if ( head == null ) {
+      head = node;
+    }
+    else {
+      var current = head;
+      while ( current.next != null ) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    size++;
+  }
+
+  public boolean add( int index , E element ) {
+    Objects.checkIndex( index, size );
+    if ( index == 0 ) {
+      addFirst( element );
+      return true;
+    }
+    var node = new Node<>(element);
+    var current = head;
+    for (int i = 1; i < index ; i++) {
+      current = current.next;
+    }
+    node.next = current.next;
+    current.next = node;
+    size++;
+    return true;
+  }
+
+  public E getFirst() {
+    if ( head == null ) {
+      throw  new NoSuchElementException("LinkedList is empty");
+    }
+    return head.element;
+  }
+
+  public E getLast() {
+    if ( head==null ){
+      throw new NoSuchElementException("LinkedList is null");
+    }
+    var current = head;
+    while ( current.next != null ) {
+      current = current.next;
+    }
+    return current.element;
+  }
+
+  public E get( int index ) {
+    Objects.checkIndex( index , size );
+    if ( index == 0 ) {
+      return getFirst();
+    }
+
+    var current = head;
+
+    for (int i = 1; i <= index; i++) { // i = 4
+      current = current.next;
+    }
+
+    return current.element;
+  }
+
+
+
+
+  public E remove( int index ) {
+
+    Objects.checkIndex(index, size);
+
+    if ( index == 0 ) {
+      var node = head;
+      head = head.next;
+      size--;
+      return node.element;
+    }
+
+    var current = head;
+    for (int i = 1; i < index ; i++) {
+      current = current.next;
+    }
+
+    var node = current.next;
+    current.next = current.next.next;
+    size--;
+    return node.element;
+  }
+
+  public boolean remove( E element ) {
+    for ( int i = 0; i < size; i++ ) {
+      if ( Objects.equals( element, this.get( i ) ) ) {
+        remove( i );
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean set(int index, E element ) {
+    Objects.checkIndex(index, size);
+    if ( index == 0 ) {
+      head.element = element;
+      return true;
+    }
+    var current = head;
+    for (int i = 0; i < index ; i++) {
+      current = current.next;
+    }
+    current.element = element;
+    return true;
+  }
+
+
+
+  public int size(){
+    return this.size;
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder("[");
+    var current = head;
+    builder.append( current.element );
+    while ( current.next != null ) {
+      current = current.next;
+      builder.append(",");
+      builder.append( current.element );
+    }
+    builder.append("]");
+    return builder.toString();
+  }
+
+  static class Node<E> {
+    E element;
+    Node<E> next;
+
+    public Node(E element) {
+      this.element = element;
+    }
+  }
+}
