9818dfebf87419992a73912694eadbe219bf2eaf
Ogabek <ilhomjonovo200@gmail.com>
2025-09-26 10:12:07 +0500

    first commit

--- src/lesson_4/homeworks/homework2/App.java
+++ src/lesson_4/homeworks/homework2/App.java
@@ -3,22 +3,31 @@ package lesson_4.homeworks.homework2;
 import java.util.ArrayList;
 
 public class App {
+
   public static void main(String[] args) {
 
     SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
 
-    linkedList.add( 8 );
-    linkedList.add( 3 );
-    linkedList.add( 6 );
-    linkedList.add( 9 );
+    linkedList.add(8);
+    linkedList.add(3);
+    linkedList.add(6);
+    linkedList.add(9);
 
-    System.out.println(linkedList);
-    /*Integer c = 6;
-    linkedList.remove(c);
+
+   /* System.out.println(linkedList);
+    Integer c = 11;
+    System.out.println(linkedList.remove(c));
+    linkedList.set(2, 123);
     System.out.println(linkedList);*/
 
-    linkedList.set(1,5);
+    linkedList.set(2,12);
     System.out.println(linkedList);
+
+
+    /*linkedList.set(1,5);
+    System.out.println(linkedList);
+    System.out.println(linkedList.getFirst());
+    System.out.println(linkedList.get(3));*/
 
     // addFirst() 👌
     // add( index, E ) 👌
@@ -27,15 +36,6 @@ public class App {
     // E remove( index ) 👌
     // boolean remove( Object ) 👌
     // set( index , E ) 👌
-
-
-
-
-
-
-
-
-
 
   }
 }
---
+++ src/lesson_5/App.java
@@ -0,0 +1,9 @@
+package lesson_5;
+
+public class App {
+
+  public static void main(String[] args) {
+
+  }
+
+}
