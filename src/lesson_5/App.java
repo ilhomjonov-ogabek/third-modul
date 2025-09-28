a69dde16702beef2104326b8ce6bf7f950cb89b3
Ogabek <ilhomjonovo200@gmail.com>
2025-09-28 16:14:47 +0500

    first commit

--- src/lesson_5/App.java
+++ src/lesson_5/App.java
@@ -1,9 +1,34 @@
 package lesson_5;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class App {
 
   public static void main(String[] args) {
 
+    Student student = new Student("Ali","Aliyev",20);
+    Student student2 = new Student("Ali","Aliyev",20);
+
+    Set<Student> set = new HashSet<>();
+    set.add(student);
+    set.add(student2);
+
+    System.out.println(student.hashCode());
+    System.out.println(student2.hashCode());
+
+    for (Student s : set) {
+      System.out.println(s.toString());
+
+    }
+    NavigableSet<Student> navigableSet = new TreeSet<>();
+
+    HashSet<Student> hashSet = new HashSet<>();
+
+    LinkedHashSet<Student> linkedHashSet = new LinkedHashSet<>();
   }
 
 }
