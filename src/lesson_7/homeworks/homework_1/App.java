2b7fae6a681f82a7b55c5ae9603269f61c8e2d9c
Ogabek <ilhomjonovo200@gmail.com>
2025-10-03 11:12:46 +0500

    first commit

---
+++ src/lesson_7/homeworks/homework_1/App.java
@@ -0,0 +1,24 @@
+package lesson_7.homeworks.homework_1;
+
+import java.util.Scanner;
+
+public class App {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    MapTest<Integer, Car> mapTest = new MapTest<>();
+    mapTest.put(1000,new Car("Mers","black","mers"));
+    mapTest.put(2000,new Car("Mers","white","mers"));
+    mapTest.put(1500,new Car("BMW","black","bmw"));
+    mapTest.put(3000,new Car("AUDI","black","audi"));
+    mapTest.put(5000,new Car("Mers","black","mers"));
+
+    System.out.print("Enter price of car: ");
+    int price = sc.nextInt();
+   MapTest.printCars(price);
+  }
+
+
+  }
+
+}
